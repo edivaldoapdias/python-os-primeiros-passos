ac7fd2410dc45a8db7733930a0df35daa124f9b8
edivaldo <nenebsgi@hotmail.com>
2023-08-10 21:29:32 -0300

    Primeira mudança

---
+++ funcoes_entrada_saida.py
@@ -0,0 +1,1 @@
+input("Informe o seu nome:")
---
+++ variaves_e_constantes.py
@@ -0,0 +1,12 @@
+nome = 'Edivaldo'
+
+idade = 43
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = [ "SP", "RJ" , "MG" , "BA" ]
+
+print(BRAZILIAN_STATES)
+
