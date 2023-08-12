d006e4e98b827f7315b63fad656b10625641311f
edivaldo <nenebsgi@hotmail.com>
2023-08-12 11:30:33 -0300

    Quinta Mudança

---
+++ operadore_associaçao.py
@@ -0,0 +1,11 @@
+curso = "Curso de Python"
+frutas = ["laranja","uva","limão"]
+saques = [1500,100]
+
+print("Python" in curso)
+
+print("python" in curso)
+
+print("maçã" not in frutas)
+
+print(200 in saques)
